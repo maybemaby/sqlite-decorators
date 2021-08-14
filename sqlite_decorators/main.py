bc7be2bf13c84d3aca524eba0e42183801adbcbf
maybemaby <74226064+maybemaby@users.noreply.github.com>
2021-08-13 22:39:58 -0700

    Changed create_table to make dicts into tables

--- sqlite_decorators/main.py
+++ sqlite_decorators/main.py
@@ -1,12 +1,40 @@
 import sqlite3, functools
 
+
+def column_strings(table_dict: dict) -> str:
+    """Helper function to take the dictionary for create_table and 
+    make the string for SQL execution.
+    """
+    column_dict = table_dict.copy()
+    column_dict.pop('name')
+    output_list = []
+    for name, col_type in column_dict.items():
+        output_list.append(f"{name} {col_type}")
+    return ', '.join(output_list)
+
+
 def create_table(assigned_cursor: sqlite3.Cursor, db_connection: sqlite3.Connection):
+    """Decorator that takes a sqlite3 Cursor object and sqlite3 Connection and
+    creates a table from a dictionary returning from an attached function. 
+
+    Dictionary takes the form:
+    {
+        "name": "table_name",
+        "column1_name": "column1_type",
+        "column2_name": "column2_type"
+    }
+
+    :param assigned_cursor: [description]
+    :type assigned_cursor: sqlite3.Cursor
+    :param db_connection: [description]
+    :type db_connection: sqlite3.Connection
+    """
     def decorator_create_table(func):
         @functools.wraps(func)
         def wrapper_create_table(*args, **kwargs):
-            table_name = func(*args, **kwargs)
-            assigned_cursor.execute(f'CREATE TABLE {table_name} (test text)')
+            table_data = func(*args, **kwargs)
+            assigned_cursor.execute(f'CREATE TABLE {table_data["name"]} ({column_strings(table_data)})')
             db_connection.commit()
-            return table_name
+            return table_data
         return wrapper_create_table
     return decorator_create_table
--- tests/test_sqlite_decorators.py
+++ tests/test_sqlite_decorators.py
@@ -7,10 +7,19 @@ def test_version():
 
 def test_create_table(test_db):
     @create_table(test_db.cursor, test_db.connection)
-    def output_string():
-        return "Table1"
-    example_str = output_string()
-    assert example_str == "Table1"
+    def table_template():
+        return {
+            "name": "Table1",
+            "column1": "TEXT",
+            "column2": "INT"
+            }
+    example_dict = table_template()
+    # Make sure the decorator still returns the normal function output.
+    assert example_dict == {
+            "name": "Table1",
+            "column1": "TEXT",
+            "column2": "INT"
+            }
     tables = test_db.cursor.execute("""SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%';""").fetchone()
     assert len(tables) == 1
     assert tables[0] == "Table1"
